e501b820b6c38cf44d5a86c8778c47f17bcbd4d3
Menshons <menshons@gmail.com>
2022-07-29 14:42:14 +0200

    Init

---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const LoginForm: React.FC = () => {
+    const [test, setTest] = useState(5);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LoginForm;
