f9ef17ed12ddfb2d45216ca70a1114f9ca89265b
Menshons <menshons@gmail.com>
2022-08-22 20:08:29 +0200

    ESLint install

--- src/pages/project/profile/FriendsPage.tsx
+++ src/pages/project/profile/FriendsPage.tsx
@@ -1,18 +1,18 @@
-import React, {useState} from 'react';
-import {ControlPanel, ControlPanelWrapper} from "../../../components/styles/content/Content";
-import Button from "../../../components/styles/material-ui/components/Button";
-import Dialog from "../../../components/styles/material-ui/components/Dialog";
-import AddFriend from "../../../components/profile/AddFriend";
-import FriendsList from "../../../components/profile/FriendsList";
+import React, { useState } from 'react'
+import { ControlPanel, ControlPanelWrapper } from '../../../components/styles/content/Content'
+import Button from '../../../components/styles/material-ui/components/Button'
+import Dialog from '../../../components/styles/material-ui/components/Dialog'
+import AddFriend from '../../../components/profile/AddFriend'
+import FriendsList from '../../../components/profile/FriendsList'
 
 const FriendsPage = () => {
-    const [openAddFriendModal, setOpenAddFriendModal] = useState<boolean>(false);
+  const [openAddFriendModal, setOpenAddFriendModal] = useState<boolean>(false)
 
-    return (
+  return (
         <>
             <ControlPanelWrapper>
                 <ControlPanel>
-                    <li><Button variant="contained" color="success" onClick={() =>setOpenAddFriendModal(true)}>Přidat přítele</Button></li>
+                    <li><Button variant="contained" color="success" onClick={() => setOpenAddFriendModal(true)}>Přidat přítele</Button></li>
                 </ControlPanel>
                 <Dialog open={openAddFriendModal} onClose={() => setOpenAddFriendModal(false)}>
                     <AddFriend />
@@ -20,7 +20,7 @@ const FriendsPage = () => {
             </ControlPanelWrapper>
             <FriendsList />
         </>
-    );
-};
+  )
+}
 
-export default FriendsPage;
+export default FriendsPage
